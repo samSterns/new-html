dc6b63eea886c245865e31a379eacd389f303fc6
samSterns <sam.g.sterns@gmail.com>
2019-09-18 21:07:03 -0700

    day 3 java

---
+++ app.js
@@ -0,0 +1,31 @@
+// alert('hello world') 
+const dog = {
+    name: 'fido',
+    color: 'brown',
+    weight: 50,
+}
+console.log(dog.name);
+dog.name = 'rover'
+
+if(dog.name === 'rover') {
+    console.log('red rover, come over')
+
+} else if(dog.name === 'spot') {
+    console.log('you are a normal dog');
+} else if(dog.name === time) {
+    console.log('that is a person name')
+}
+
+// count from 0 to ten
+const veggies = ['carrot', 'potato', 'turnip']
+for(let i =0; i<=2; i++) {
+    console.log(i);
+    if (i === 2) {
+    console.log(veggie[i]);
+}
+function showAlert() {
+    alert('you clicked a button')
+}
+function logHello() {
+    console.log(Hello);
+}
